784fe6e5382deee968208750861f0b834280a37e
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-02-18 20:54:34 -0300

    ajuste

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '14e8cd984700889ac2b1096e3a16a14fa43aa207',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -431,9 +431,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '14e8cd984700889ac2b1096e3a16a14fa43aa207',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
