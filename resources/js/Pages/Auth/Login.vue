1f2ff135d7fdaa4b3bc430a70250b0f91b123ea7
Sávio Gomes da Silva <saviogomedasilva@yahoo.com>
2025-03-20 16:02:56 -0300

    ajuste login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -73,15 +73,15 @@ const submit = () => {
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
-                
+
             </div>
 
             <div class="flex items-center justify-end mt-4">
-    <a :href="route('register')" 
-       class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        Registrar
-    </a>
-</div>
+                <a :href="route('register')"
+                    class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Registrar
+                </a>
+            </div>
 
         </form>
     </AuthenticationCard>
